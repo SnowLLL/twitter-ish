c210d8c6fe9748b12cb60cebf2993210a8537d06
wiki <liuxueworking@gmail.com>
2021-04-16 12:31:21 -0700

    add view link button for detail pages

--- tweetme-web/src/tweets/detail.js
+++ tweetme-web/src/tweets/detail.js
@@ -7,7 +7,20 @@ export function Tweet(props) {
     const [actionTweet, setActionTweet] = useState(props.tweet ? props.tweet : null)
     const className = props.className ? props.className : 'col-10 mx-auto col-md-6'
 
-    // display 
+    const path = window.location.pathname
+    var match = path.match(/(?<tweetid>\d+)/) // /(?P<id>\d+)/ in python
+    // match = e.g ["111", "111", index: 1, input: "/111", groups: {…}]
+    const urlTweetId = match ? match.groups.tweetid : -1 // if match === true(exist) return ? or :
+
+    // handle view link button
+    const isDetail = `${tweet.id}` === `${urlTweetId}` // return true or false
+    const handleDetailLink = (event) => {
+        event.preventDefault()
+        // handle link redirection
+        window.location.href = `${tweet.id}`
+    }
+
+    // display parent tweet
     const handlePerformAction = (newActionTweet, status) => {
         if (status === 200) {
             setActionTweet(newActionTweet)
@@ -24,13 +37,17 @@ export function Tweet(props) {
                 < p >{tweet.id} - {tweet.content}</p >
                 < ParentTweet tweet={tweet} />
             </div>
-            {(actionTweet && hiddenActions !== true) &&
-                <div className="btn btn-primary">
-                    <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{ type: 'like', display: 'Likes' }} />
-                    <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{ type: 'unlike', display: 'UnLikes' }} />
-                    <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{ type: 'retweet', display: 'Retweet' }} />
-                </div>
-            }
+
+            <div className='btn btn-group'>
+                {(actionTweet && hiddenActions !== true) &&
+                    <React.Fragment>
+                        <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{ type: 'like', display: 'Likes' }} />
+                        <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{ type: 'unlike', display: 'UnLikes' }} />
+                        <ActionBtn tweet={actionTweet} didPerformAction={handlePerformAction} action={{ type: 'retweet', display: 'Retweet' }} />
+                    </React.Fragment>}
+                {isDetail === true ? null : <button id="viewLink" className="btn btn-outline-primary btn-sm" onClick={handleDetailLink}> View </button>}
+            </div>
+
         </div >
 
     );
