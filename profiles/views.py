a7e89c3de30a9c0ce85021e8b42d13c4acb09a80
wiki <liuxueworking@gmail.com>
2021-04-19 22:23:47 -0700

    add update profile info section

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
 from .models import Profile
-
+from .forms import ProfileForm
 # Create your views here.
 
 
@@ -17,3 +17,33 @@ def profile_detail_view(request, username, *args, **kwargs):
     }
     # get the profile for the passed username
     return render(request, 'profiles/detail.html', context)
+
+
+def profile_update_view(request, *args, **kwargs):
+    if not request.user.is_authenticated:
+        # after login go to /profile/update
+        return redirect('/login?next=/profile/update')
+    # create a form like a Profile model
+    # b/c the model name is Profile and is one to one field
+    user_data = {
+        'first_name': request.user.first_name,
+        'last_name': request.user.last_name,
+        'email': request.user.email,
+    }
+    form = ProfileForm(request.POST or None,
+                       instance=request.user.profile, initial=user_data)
+    if form.is_valid():
+        # save form but not to database
+        profile_obj = form.save(commit=False)
+        # update user's info
+        request.user.first_name = form.cleaned_data.get('first_name')
+        request.user.last_name = form.cleaned_data.get('last_name')
+        request.user.email_address = form.cleaned_data.get('email')
+        request.user.save()
+        profile_obj.save()
+    context = {
+        'form': form,
+        'btn_label': 'Save',
+        'title': 'Update Profile'
+    }
+    return render(request, 'profiles/updateprofiles.html', context)
