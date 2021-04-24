a92648b230dd2c908a801f0d0a13bcd610e84423
wiki <liuxueworking@gmail.com>
2021-04-23 21:28:10 -0700

    reorganize profile api view

--- profiles/api/urls.py
+++ profiles/api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import user_follow_view, profile_detail_api_view
+from .views import profile_detail_api_view  # , user_follow_view
 
 
 # base endPoint /api/profiles/
 urlpatterns = [
     # username should be the same as props in user_follow_view
-    path('<str:username>/follow', user_follow_view),
+    path('<str:username>/follow', profile_detail_api_view),
     path('<str:username>/', profile_detail_api_view),
 ]
--- profiles/api/views.py
+++ profiles/api/views.py
@@ -20,38 +20,49 @@ User = get_user_model()
 #     currentUser = request.user
 
 #     return Response({}, status=400)
-@ api_view(['GET'])
+@ api_view(['GET', 'POST'])
 def profile_detail_api_view(request, username, *args, **kwargs):
     qs = Profile.objects.filter(user__username=username)
     if not qs.exists():
         return Response({"detail": "User is not found"}, status=404)
     # the first one of objects
     profile_obj = qs.first()
-    data = PublicProfileSerializer(
+    if request.method == "POST":
+        data = request.data or {}  # if not request.data being sent, it will give empty
+        action = data.get("action")
+        if profile_obj.user != request.user:
+            # toggle
+            if action == 'follow':
+                profile_obj.followers.add(request.user)
+            elif action == 'unfollow':
+                profile_obj.followers.remove(request.user)
+            else:
+                pass
+    serializer = PublicProfileSerializer(
         instance=profile_obj, context={'request': request})
-    return Response(data.data, status=200)
+    return Response(serializer.data, status=200)
 
 
-@ api_view(['GET', 'POST'])
-@ permission_classes([IsAuthenticated])
-def user_follow_view(request, username, *args, **kwargs):
-    me = request.user
-    if me.username == username:
-        return Response({'count': me.profile.followers.all().count()}, status=200)
-    otherUser = User.objects.filter(username=username)
-    if otherUser.exists() == False:
-        raise Http404()
-    # profile = Profile.objects.filter(user__username__iexact=username).first() #another way to find profile
-    profile = otherUser.first().profile
-    data = request.data or {}  # if not request.data being sent, it will give empty
-    action = data.get("action")
-    # toggle
-    if action == 'follow':
-        profile.followers.add(me)
-    elif action == 'unfollow':
-        profile.followers.remove(me)
-    else:
-        pass
-    data = PublicProfileSerializer(
-        instance=profile, context={'request': request})
-    return Response(data.data, status=200)
+# @ api_view(['GET', 'POST'])
+# @ permission_classes([IsAuthenticated])
+# def user_follow_view(request, username, *args, **kwargs):
+#     me = request.user
+#     if me.username == username:
+#         return Response({'count': me.profile.followers.all().count()}, status=200)
+#     otherUser = User.objects.filter(username=username)
+#     if otherUser.exists() == False:
+#         raise Http404()
+#     # profile = Profile.objects.filter(user__username__iexact=username).first() #another way to find profile
+#     profile = otherUser.first().profile
+#     data = request.data or {}  # if not request.data being sent, it will give empty
+#     action = data.get("action")
+#     # toggle
+#     if action == 'follow':
+#         profile.followers.add(me)
+#     elif action == 'unfollow':
+#         profile.followers.remove(me)
+#     else:
+#         pass
+#     serializer = PublicProfileSerializer(
+#         instance=profile, context={'request': request})
+#     return Response(serializer.data, status=200)
