1736026a6584abc8d3064461fb09c286876e8214
SnowLLL <73144861+SnowLLL@users.noreply.github.com>
2021-04-29 12:10:49 -0700

    Update settings.py
    Why set Host to this

--- twitter/settings.py
+++ twitter/settings.py
@@ -92,6 +92,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
         'HOST': '127.0.0.1',
+        # because you used 127.0.0.1 when fetch data from React App
     }
 }
 
